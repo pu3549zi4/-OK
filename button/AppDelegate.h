474b73f16c807c73bb20104668e95fbc8ff34c21
Xmart <xmart@XmartdeMac-mini.local>
2015-10-10 14:36:54 +0800

    Initial Commit

---
+++ button/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  button
+//
+//  Created by Xmart on 15/10/10.
+//  Copyright (c) 2015年 Xmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ button/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  button
+//
+//  Created by Xmart on 15/10/10.
+//  Copyright (c) 2015年 Xmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
